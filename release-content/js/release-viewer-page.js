69ecb2c4dbc2a1c58b34ee965c2a9f4a7e313fb4
Dino Chiesa <dchiesa@apigee.com>
2016-01-27 18:04:36 -0800

    update release viewer

--- release-content/js/release-viewer-page.js
+++ release-content/js/release-viewer-page.js
@@ -4,11 +4,12 @@
 // page logic for release-content.html
 //
 // created: Thu Oct  1 13:37:31 2015
-// last saved: <2016-January-27 15:58:58>
+// last saved: <2016-January-27 18:00:17>
 
 var baseUrl = 'https://deecee-test.apigee.net/release-content';
 var re1 = new RegExp('^\\[[A-Z]{2,6}-[1-9][0-9]{2,5}\\] *');
 var re2 = new RegExp('^[-\\|] *');
+var re3 = new RegExp('desc-([a-zA-Z]+-[0-9]+)');
 
 function linkify(id) {
   return 'https://apigeesc.atlassian.net/browse/' + id;
@@ -34,6 +35,7 @@ function transformResponse (a) {
   return a.map(function(item){
     return {
       id: '<a href="'+ linkify(item.id) + '">' +item.id + '</a>',
+      description: '<div class="item-description" id="desc-' + item.id.toLowerCase() + '"></div>',
       commits: formatDescription(item.commits)
     };
   });
@@ -57,15 +59,30 @@ function transformResponse (a) {
 //     } );
 // }
 
+function fillDetails(ix, elt) {
+  var $elt = $(elt);
+  var id = re3.exec($elt.attr('id'))[1].trim().toUpperCase();
+  var settings = {
+        type: 'GET',
+        url: baseUrl + '/jira/' + id,
+        dataType: 'json',
+        cache: 'false'
+      };
+  var jqxhr = $.ajax(settings)
+    .done(function() {
+      var o = jqxhr.responseJSON; // parsed object
+      $($elt).html(o.fields.summary);
+    });
+}
 
 function fetchContent(event) {
   if (event) { event.preventDefault(); }
   var release = $('#release').val().trim();
   var settings = {
         type: 'GET',
-        url: baseUrl + '/' + release,
+        url: baseUrl + '/rel/' + release,
         dataType: 'json',
-        cache: 'true'
+        cache: 'false'
       };
 
   var jqxhr = $.ajax(settings)
@@ -74,11 +91,17 @@ function fetchContent(event) {
       $('#rtable').DataTable( {
         data: transformResponse(a),
         columns: [
-          { data: "id", className: 'dt-collapse' },
-          { data: "commits", className: 'dt-widen' }
+          { data: 'id', className: 'dt-collapse' },
+          { data: 'description', className: 'dt-widen' },
+          { data: 'commits', className: 'dt-widen' }
         ]
       } );
       $('#rtable').css('display', '');
+
+      // now lazily fill the descriptions:
+      $('div.item-description').each(fillDetails);
+      $('#rtable').on( 'length.dt', pageRefresh);
+      $('#rtable').on( 'page.dt', pageRefresh);
     })
     .fail(function() {
       alert( "error fetching content: " + jqxhr.statusCode() + ' ' + jqxhr.statusText);
@@ -86,11 +109,15 @@ function fetchContent(event) {
     .always(function() {
       // alert( "complete" );
     });
-
-
-
 }
 
+
+function pageRefresh() {
+  setTimeout( function() {
+    var $items = $('div.item-description');
+    $items.each(fillDetails);
+  }, 350);
+}
 
 
 // function rowAppender($div) {
