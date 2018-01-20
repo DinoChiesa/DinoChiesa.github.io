c6db70389f717cf77f07e03711f236b4dd1e2db4
Dino Chiesa <dpchiesa@hotmail.com>
2018-01-19 16:37:05 -0800

    update load generator page

--- load-generator/js/load-generator-page.js
+++ load-generator/js/load-generator-page.js
@@ -12,6 +12,13 @@
   var html5AppId = html5AppId || "67B53CD3-AD0A-4D58-8DE7-997EBC7B3ED1";   // for localstorage
   var runState = 0;
   var timeoutId = null;
+  var oneHeaderLine =
+    '<div class="form-group col-sm-8 one-header">\n' +
+    '  <input autocomplete="off" class="form-control http-header-name" value="" placeholder="Header Name"/>\n' +
+    '  <input autocomplete="off" class="form-control http-header-value" value="" placeholder="Header Value"/>\n' +
+    '  <button class="btn btn-default btn-outline remove-header" title="delete">X</button>\n' +
+    '</div>\n' +
+    '<div class="clearfix"></div>';
   var Gaussian = function(mean, stddev) {
         /*
           Function normal.
@@ -37,8 +44,6 @@
         };
       };
 
-
-
   function onInputChanged() {
     var $$ = $(this), name = $$.attr('id'), value = $$.val();
     model[name] = value;
@@ -47,7 +52,15 @@
   function onSelectChanged() {
     var $$ = $(this), name = $$.attr('name');
     $$.find("option:selected").each(function() {
-      model[name] = $( this ).text();
+      var text = $( this ).text().toLowerCase(),
+          $payload = $("#payload");
+      model[name] = text;
+      if (text === 'post' || text === 'put') {
+        $( "#option-tabs" ).tabs( "option", "disabled", false );
+      }
+      else if (text === 'get') {
+        $( "#option-tabs" ).tabs( "option", "disabled", [1] );
+      }
     });
   }
 
@@ -95,8 +108,8 @@
     //sleepTimeInMs += gaussian.next();
     sleepTimeInMs = Math.floor(sleepTimeInMs);
     $('#status').html(//'runs per hour('+ runsPerHour +') ' +
-                       'sleep(' + sleepTimeInMs + 'ms) ' +
-                       'wake(' +  new Date(now.valueOf() + sleepTimeInMs).toString().substr(16, 8) + ')');
+      'sleep(' + sleepTimeInMs + 'ms) ' +
+      'wake(' +  new Date(now.valueOf() + sleepTimeInMs).toString().substr(16, 8) + ')');
     return sleepTimeInMs;
   }
 
@@ -104,7 +117,7 @@
     if (runState !== 1) { return 0; }
 
     var linkUrl = $('#endpoint').val().trim();
-    var method = $('#method option:selected').val();
+    var method = $('#method option:selected').val().toLowerCase();
     var startTime = new Date();
     var options = {
           url : linkUrl,
@@ -112,6 +125,11 @@
           headers: { },
           success: function(data, textStatus, jqXHR) {
             incrementCount('#requestcount');
+            $( "#response" ).html("<pre>" +
+                                  jqXHR.status + " " + jqXHR.statusText + "\n" +
+                                  jqXHR.getAllResponseHeaders() + "\n" +
+                                  jqXHR.responseText +
+                                  "</pre>");
             if (runState == 1) {
               timeoutId = window.setTimeout(invokeOneCall, getSleepTime(startTime));
             }
@@ -124,42 +142,61 @@
           }
         };
 
-    if (method !== 'GET') {
-      options.data = {
-        something: 'abcdefg',
-        whatever: model.method
-      };
-      options.headers['content-type'] = 'application/json';
+    if (method !== 'get') {
+      options.data = $('#txt-payload').val();
     }
 
-    // NB: This call will fail if the server does not include CORS headers in the response
+    var $container = $('#headers');
+    $container.find('.one-header').each(function(ix, element){
+      var name = $(this).find('.http-header-name').val();
+      var value = $(this).find('.http-header-value').val();
+      options.headers[name] = value;
+    });
+
+    // NB: This call may fail if the server does not include CORS headers in the response
     //console.log(method + ' ' + linkUrl);
     $.ajax(options);
   }
 
   function updateRunState(event) {
     var $ss = $('#startstop');
+    var $r = $('#reset');
     var state = $ss.attr('data_state');
     if (state == 'running') {
       runState = 0;
       if (timeoutId) {
         window.clearTimeout(timeoutId);
         timeoutId = null;
-      $('#status').html('stopped.');
+        $('#status').html('stopped.');
       }
       $ss.attr('data_state', 'stopped');
       $ss.addClass('btn-success');
       $ss.removeClass('btn-danger');
       $ss.html('Start');
+      $r.removeAttr("disabled");
     }
     else {
       runState = 1;
       $('#status').html('running...');
       $ss.attr('data_state', 'running');
       timeoutId = setTimeout(invokeOneCall, 2);
+      $r.attr("disabled", "disabled");
       $ss.html('Stop');
       $ss.addClass('btn-danger');
       $ss.removeClass('btn-success');
+    }
+    if (event)
+      event.preventDefault();
+  }
+
+  function resetCounts(event) {
+    var $ss = $('#startstop');
+    var state = $ss.attr('data_state');
+    if (state !== 'running') {
+      var $c = $('#requestcount');
+      $c.html('0');
+      $c = $('#errorcount');
+      $c.html('0');
     }
     if (event)
       event.preventDefault();
@@ -186,13 +223,32 @@
       });
   }
 
+  function addOneHeader (event) {
+    var $container = $('#headers');
+    $container.find('button.remove-header').unbind('click'); // unbind click handlers
+    $( "#add-header" ).before(oneHeaderLine);
+    $container.find('button.remove-header').click(removeHeader); // query again
+    if (event)
+      event.preventDefault();
+  }
+
+  function removeHeader(event) {
+    var $source = $(this);
+    $source.parent().remove();
+    if (event)
+      event.preventDefault();
+  }
+
   $(document).ready(function() {
-    //$( "form input[type='text']" ).change(onInputChanged);
+    // $( "form input[type='text']" ).change(onInputChanged);
+    $( "#option-tabs" ).tabs({ active: 0 });
+    $( "#option-tabs" ).tabs( "option", "disabled", [ 1 ] ); // initially
     $( "form input[type='url']" ).change(onInputChanged);
     $( "form select" ).change(onSelectChanged);
-    $( "form button" ).click(updateRunState);
+    $( "#add-header" ).click(addOneHeader);
+    $( "form #startstop" ).click(updateRunState);
+    $( "form #reset" ).click(resetCounts);
     populateFormFieldsFromLocalStorage();
   });
-
 
 }());
