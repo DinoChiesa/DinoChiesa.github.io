e3e450305326a0d41efb96603e361d07a342a224
Dino Chiesa <dpchiesa@hotmail.com>
2019-02-21 15:41:00 -0800

    clear output when signing out

--- next2019-hyb211/js/goog-signin-page.js
+++ next2019-hyb211/js/goog-signin-page.js
@@ -20,6 +20,8 @@
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+      var elt = document.getElementById("output");
+      elt.innerHTML = '';
       console.log('User signed out.');
       showSignout(false);
     });
