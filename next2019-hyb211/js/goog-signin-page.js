3a6a17e306e7e60987aa1c7c4212064a36dffcc1
Dino Chiesa <dpchiesa@hotmail.com>
2019-02-21 15:53:35 -0800

    eliminate given and family name

--- next2019-hyb211/js/goog-signin-page.js
+++ next2019-hyb211/js/goog-signin-page.js
@@ -20,7 +20,7 @@
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      var elt = document.getElementById("output");
+      var elt = document.getElementById('output');
       elt.innerHTML = '';
       console.log('User signed out.');
       showSignout(false);
@@ -32,8 +32,8 @@
     var profile = googleUser.getBasicProfile();
     var html = oneDiv('ID',  profile.getId()) +
       oneDiv('Full Name', profile.getName()) +
-      oneDiv('Given Name',  profile.getGivenName()) +
-      oneDiv('Family Name', profile.getFamilyName()) +
+      //oneDiv('Given Name',  profile.getGivenName()) +
+      //oneDiv('Family Name', profile.getFamilyName()) +
       oneDiv("Email", profile.getEmail()) +
       oneDiv('Image', '<img src="' + profile.getImageUrl() + '">');
 
@@ -41,15 +41,11 @@
     var id_token = googleUser.getAuthResponse().id_token;
     html += oneDiv("ID Token", id_token);
     elt.innerHTML = html;
-    var signout = document.getElementById("signout");
-    if (signout) {
-      signout.classList.add('visible');
-      signout.classList.remove('hidden');
-    }
+    showSignout(true);
   }
 
   function showSignout(visible) {
-    var signout = document.getElementById("signout");
+    var signout = document.getElementById('signout');
     if (signout) {
       signout.classList.add(visible?'visible':'hidden');
       signout.classList.remove(visible?'hidden':'visible');
