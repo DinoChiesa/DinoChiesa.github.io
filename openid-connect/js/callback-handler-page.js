16017bbf764cc82c97e3662548d0b3d246e5d129
Dino Chiesa <dchiesa@google.com>
2019-08-29 17:37:56 -0700

    update goog OIDC reply page

--- openid-connect/js/callback-handler-page.js
+++ openid-connect/js/callback-handler-page.js
@@ -4,8 +4,9 @@
 // for callback-handler.html
 //
 // created: Thu Oct  1 13:37:31 2015
-// last saved: <2018-March-13 09:47:08>
-
+// last saved: <2019-August-29 17:37:10>
+/* jshint esversion: 9 */
+/* global $, atob */
 
 function decodeToken(matches) {
   if (matches.length == 4) {
@@ -22,7 +23,7 @@ function decodeToken(matches) {
 }
 
 function formatIdToken() {
-  var $$ = $( '#id_token-value div.cb-value' ),
+  let $$ = $( '#id_token-value div.cb-value' ),
       text = $$.text(),
       re1 = new RegExp('^([^\\.]+)\\.([^\\.]+)\\.([^\\.]+)$');
   if (text) {
@@ -32,9 +33,31 @@ function formatIdToken() {
   }
 }
 
+function copyButtonHtml(targetElementId) {
+  let html = '<button type="button" title="copy to clipboard" class="btn btn-default btn-md btn-copy" ' +
+    'data-target="'+targetElementId+'" title="copy to clipboard">\n' +
+    '  <span class="glyphicon glyphicon-copy"></span>\n' +
+    '</button>\n';
+  return html;
+}
+
+function copyToClipboard(event) {
+  let $elt = $(this),
+      sourceElement = $elt.data('target'),
+      // grab the element to copy
+      $source = $('#' + sourceElement),
+      // Create a temporary hidden textarea.
+      $temp = $("<textarea>"),
+      textToCopy = ($source[0].tagName == 'TEXTAREA') ? $source.val() : $source.text();
+
+  $("body").append($temp);
+  $temp.val(textToCopy).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
 
 $(document).ready(function() {
-  var search = window.location.hash,
+  let search = window.location.hash,
       hash = {},
       fnStartsWith = function(s, searchString, position) {
         position = position || 0;
@@ -46,7 +69,7 @@ $(document).ready(function() {
   }
 
   search.split('&').forEach(function(item){
-    var e = item.split('=');
+    let e = item.split('=');
     if (e[0] && e[0] !== '') {
       if (fnStartsWith(e[0], '#')) { e[0] = e[0].substring(1); }
       hash[e[0]] = decodeURIComponent(e[1]);
@@ -59,9 +82,13 @@ $(document).ready(function() {
 
   Object.keys(hash).forEach(function(key){
     if (key) {
-      var $newdiv = $("<div id='"+ key +"-value' class='cb-element'/>");
-      $newdiv.addClass("cb-clearfix");
-      $newdiv.html('<div class="cb-label">' + key + ':</div><div class="cb-value">' + hash[key] + '</div>');
+      let $newdiv = $("<div id='"+ key +"-value' class='cb-element cb-clearfix'/>"),
+          valueId = 'val-' + Math.random().toString(36).substring(2, 15),
+          html = {
+            label : '<div class="cb-label">' + key + ':' + copyButtonHtml(valueId) +'</div>',
+            value : '<div id="' + valueId + '"class="cb-value">' + hash[key] + '</div>'
+          };
+      $newdiv.html(html.label + html.value);
       $$.append($newdiv);
       if (key == 'id_token') {
         $newdiv.append("<div id='id_token-decoded' class='jwt-decoded'/>");
@@ -69,6 +96,8 @@ $(document).ready(function() {
     }
   });
 
-  setTimeout(formatIdToken, 2600);
+  $( '.btn-copy' ).on('click', copyToClipboard);
+
+  setTimeout(formatIdToken, 2100);
 
 });
