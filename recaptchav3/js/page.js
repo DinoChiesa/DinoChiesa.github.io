bf689a2606996991fff0af2ada4d3fb2e37196c5
Dino Chiesa <dchiesa@google.com>
2020-06-02 10:39:39 -0700

    cleanup page.js

--- recaptchav3/js/page.js
+++ recaptchav3/js/page.js
@@ -2,11 +2,10 @@
 // ------------------------------------------------------------------
 //
 // created: Mon Jun  1 13:00:26 2020
-// last saved: <2020-June-02 10:37:10>
+// last saved: <2020-June-02 10:39:35>
 
 /* jshint esversion:9, node:false, strict:implied */
 /* global jQuery, document, window, console, Buffer, grecaptcha */
-
 
 (function (){
   const reCAPTCHA_site_key = '6LeEA3gUAAAAAPRenCnqy8K4IuLepRPAPLNIKOY_',
@@ -21,20 +20,19 @@
   function applyRecaptchaAndSubmit(event) {
     // prevent form submit
     if (event) { event.preventDefault(); }
-    // get new token for this click
+
     try {
 
       // The browser will show an opaque promise error in this method if this
       // page is hosted on a domain which does not match the domain set for
-      // the given site_key.
+      // the given site_key. The little recaptcha badge will show a warning
+      // message.
 
       grecaptcha.execute(reCAPTCHA_site_key, {action: 'homepage'})
         .then(function(recaptchaToken) {
 
-          //alert("recaptcha token:\n "+ recaptchaToken);
-
-          // post back to an endpoint. The thing on the other end will call to
-          // google to ask for the 'score' for this token.
+          // post back to an API endpoint. The thing on the other end must call to
+          // google using the site key secret, to ask for the 'score' for this token.
           var jqxhr = $.ajax({
                 url: postbackEndpoint,
                 method: "POST",
@@ -43,6 +41,8 @@
                 dataType : 'json' // response
               })
             .done(function(responseData) {
+              // The Apigee endpoint just returns what recaptcha said.
+              // Display it on the page.
               let $ta = $('<textarea>');
               $ta.attr('class', 'results code');
               $ta.attr('spellcheck','false');
@@ -54,15 +54,7 @@
             .fail(function(response, error, exc) {
               window.alert( "error: " + exc);
             });
-          //.always(function() {
-          //  alert( "complete" );
-          //});
-
-          //$('#form1').submit();
         });
-      // .catch( e => {
-      //   alert( "exception: " + e);
-      // });
     }
     catch (e) {
       window.alert( "exception: " + e);
