0ad555a5740513e1639228e175a1990ce68b3c5c
Dino Chiesa <dpchiesa@hotmail.com>
2025-03-09 21:06:52 -0700

    refactor: Simplify link generation and remove unused evalTemplate function

--- totp/js/link-builder-page.js
+++ totp/js/link-builder-page.js
@@ -3,92 +3,97 @@
 /* jshint esversion: 8 */
 /* global $, base32 */
 
-var model = {
-      baseurl : '',
-      bcsize : '',
-      label : '',
-      secret : '',
-      base32secret : '',
-      issuer : ''
-    };
+const model = {
+  baseurl: "",
+  bcsize: "",
+  label: "",
+  secret: "",
+  base32secret: "",
+  issuer: "",
+};
 
-const html5AppId = '5FADBB91-0C35-49F6-BE3F-220B632874C3'; // for localstorage
+const html5AppId = "5FADBB91-0C35-49F6-BE3F-220B632874C3"; // for localstorage
 
-const linkTemplate = `${model.baseurl}?chs=${model.bcsize}&chld=M%7C0&cht=qr&chl=@@CHL@@`;
-//var chlTemplate = "otpauth://totp/${label}?secret=${base32secret}&issuer=${issuer}";
-const chlTemplate = "otpauth://totp/${label}?secret=${base32secret}&issuer=${issuer}";
-
-function wrapInSingleQuote(s) {return "'" + s + "'";}
-
-function evalTemplate(template, model) {
-  let s = template;
-  Object.keys(model)
-    .forEach(function(key) {
-      var pattern = "${" + key + "}", value = model[key];
-      if ((model[key]) && (value !== null) && (typeof value !== 'undefined')) {
-        window.localStorage.setItem(html5AppId + '.model.' + key, value);
-      }
-      s = s.replace(pattern,value);
-    });
-  return s;
+function wrapInSingleQuote(s) {
+  return "'" + s + "'";
 }
 
-function updateLink() {
-  let baselink = evalTemplate(linkTemplate, model);
-  model.base32secret = base32.rfc4648.encode(model.secret);
-  let chl = evalTemplate(chlTemplate, model);
-  let link = baselink.replace('@@CHL@@', encodeURIComponent(chl));
-  var extraneousDoubleSlashFinder = new RegExp('^(https?://[^/]+)//(.+)$');
-  var m = extraneousDoubleSlashFinder.exec(link);
-  if (m) { link = m[1] + '/' + m[2]; }
+// function evalTemplate(template, model) {
+//   let s = template;
+//   Object.keys(model).forEach(function (key) {
+//     var pattern = "${" + key + "}",
+//       value = model[key];
+//     if (model[key] && value !== null && typeof value !== "undefined") {
+//       window.localStorage.setItem(html5AppId + ".model." + key, value);
+//     }
+//     s = s.replace(pattern, value);
+//   });
+//   return s;
+// }
 
-  $('#totplink').text(link);
-  $('#totplink').attr('href', link);
+function updateLink() {
+  const baselink = `${model.baseurl}?chs=${model.bcsize}&chld=M%7C0&cht=qr&chl=@@CHL@@`;
+  model.base32secret = base32.rfc4648.encode(model.secret);
+  const chl = `otpauth://totp/${model.label}?secret=${model.base32secret}&issuer=${model.issuer}`;
+  const link = baselink.replace("@@CHL@@", encodeURIComponent(chl));
+  const extraneousDoubleSlashFinder = new RegExp("^(https?://[^/]+)//(.+)$");
+  const m = extraneousDoubleSlashFinder.exec(link);
+  if (m) {
+    link = m[1] + "/" + m[2];
+  }
+
+  $("#totplink").text(link);
+  $("#totplink").attr("href", link);
 }
 
 function onInputChanged() {
-  var $$ = $(this), name = $$.attr('id'), value = $$.val();
+  var $$ = $(this),
+    name = $$.attr("id"),
+    value = $$.val();
   model[name] = value;
   updateLink();
 }
 
 function onSelectChanged() {
-  var $$ = $(this), name = $$.attr('name'), values = [];
-  $$.find("option:selected" ).each(function() {
-    values.push($( this ).text());
+  var $$ = $(this),
+    name = $$.attr("name"),
+    values = [];
+  $$.find("option:selected").each(function () {
+    values.push($(this).text());
   });
   model[name] = values;
   updateLink();
 }
 
 function updateModel(event) {
-  Object.keys(model).forEach(function(key) {
-    var $item = $('#' + key), value = $item.val();
+  Object.keys(model).forEach(function (key) {
+    var $item = $("#" + key),
+      value = $item.val();
     model[key] = value;
   });
   updateLink();
-  if (event)
-    event.preventDefault();
+  if (event) event.preventDefault();
 }
 
 function excludeTransientFields(key) {
-  return key != 'base32secret'; // keep all
+  return key != "base32secret"; // keep all
 }
 
 function populateFormFields() {
   // get values from local storage, and place into the form
   Object.keys(model)
     .filter(excludeTransientFields)
-    .forEach(function(key) {
-      var value = window.localStorage.getItem(html5AppId + '.model.' + key);
-      var $item = $('#' + key);
-      if ($item.length > 0 ) {
-        if (value && value !== '') {
-          if ($item[0].tagName === 'SELECT') {
-            $item.find("option[value='"+value+"']").prop("selected", "selected");
+    .forEach(function (key) {
+      var value = window.localStorage.getItem(html5AppId + ".model." + key);
+      var $item = $("#" + key);
+      if ($item.length > 0) {
+        if (value && value !== "") {
+          if ($item[0].tagName === "SELECT") {
+            $item
+              .find("option[value='" + value + "']")
+              .prop("selected", "selected");
             $item.trigger("chosen:updated");
-          }
-          else {
+          } else {
             // value is a simple string, form field type is input.
             $item.val(value);
           }
@@ -98,35 +103,31 @@ function populateFormFields() {
 }
 
 function resetEverything(event) {
-  $('#barcodeResult').html('');
+  $("#barcodeResult").html("");
   updateModel();
-  if (event)
-    event.preventDefault();
+  if (event) event.preventDefault();
 }
 
 function showBarcode(event) {
-  $('#barcodeResult').html(`<img src="${$('#totplink').attr('href')}">`);
-  if (event)
-    event.preventDefault();
+  $("#barcodeResult").html(`<img src="${$("#totplink").attr("href")}">`);
+  if (event) event.preventDefault();
 }
 
-
-$(document).ready(function() {
-  $('.bcsize-chosen').chosen({
+$(document).ready(function () {
+  $(".bcsize-chosen").chosen({
     disable_search: true,
     no_results_text: "No matching size...",
-    allow_single_deselect: true
+    allow_single_deselect: true,
   });
 
-  $( "form input[type='text']" ).change(onInputChanged);
-  $( "form select" ).change(onSelectChanged);
-  $( "form button" ).submit(updateModel);
+  $("form input[type='text']").change(onInputChanged);
+  $("form select").change(onSelectChanged);
+  $("form button").submit(updateModel);
 
-  $( "#show-barcode" ).click(showBarcode);
-  $( "#reset-everything" ).click(resetEverything);
+  $("#show-barcode").click(showBarcode);
+  $("#reset-everything").click(resetEverything);
 
   populateFormFields();
 
   updateModel();
-
 });
